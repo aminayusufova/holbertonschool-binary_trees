abf220195d5dc6829888723b65392e421f31f839
as1an87 <as1anoff87@gmail.com>
2024-04-15 20:13:19 +0000

    create a binary tree node

--- binary_trees.h
+++ binary_trees.h
@@ -44,6 +44,7 @@ struct queue
 
 typedef  struct queue queue;
 
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 #endif
