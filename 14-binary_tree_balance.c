c7a3cd17d316d592a7386157c2e7f55306c84304
aminayusufova <158808697+aminayusufova@users.noreply.github.com>
2024-04-16 10:28:49 +0400

    updating 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,13 +8,13 @@
  */
 size_t _binary_tree_height(const binary_tree_t *tree)
 {
-        int r_height, l_height;
+	int r_height, l_height;
 
-        if (!tree)
-                return (0);
-        r_height = _binary_tree_height(tree->right);
-        l_height = _binary_tree_height(tree->left);
-        return (1 + ((r_height >= l_height) ? r_height: l_height));
+	if (!tree)
+		return (0);
+	r_height = _binary_tree_height(tree->right);
+	l_height = _binary_tree_height(tree->left);
+	return (1 + ((r_height >= l_height) ? r_height : l_height));
 }
 
 /**
@@ -22,11 +22,11 @@ size_t _binary_tree_height(const binary_tree_t *tree)
  * @tree: ptr to the root node of the binary tree
  *
  * Return: Balance factor of the binary tree,
- * or 0 if tree is null or if it's an empty tree
+ * or 0 if tree is NULL or if it's an empty tree
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-        if (!tree)
-                return (0);
-        return (_binary_tree_height(tree->left) - _binary_tree_height(tree->right));
+	if (!tree)
+		return (0);
+	return (_binary_tree_height(tree->left) - _binary_tree_height(tree->right));
 }
