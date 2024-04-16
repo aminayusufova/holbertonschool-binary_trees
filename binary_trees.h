c86c3661f3cc26e9908bd68e30e7ecdc7ead2946
aminayusufova <158808697+aminayusufova@users.noreply.github.com>
2024-04-16 10:11:24 +0400

    binary_trees.h update

--- binary_trees.h
+++ binary_trees.h
@@ -50,4 +50,5 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
 #endif
