eb1f6b699272a51374db294bb5cdbf65a12a4416
as1an87 <as1anoff87@gmail.com>
2024-04-16 19:37:30 +0000

    betty

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -4,11 +4,11 @@
  * binary_tree_preorder - function
  * @tree: val
  * @func: val
- */ 
+ */
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if(!tree || !func)
+	if (!tree || !func)
 		return;
 	func(tree->n);
 	binary_tree_preorder(tree->left, func);
