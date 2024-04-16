a0c72e12846d339f7b26c150669368034ed70f64
aminayusufova <158808697+aminayusufova@users.noreply.github.com>
2024-04-16 21:56:22 +0400

    updating 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,13 +7,13 @@
  */
 size_t _binary_tree_height(const binary_tree_t *tree)
 {
-	int r_height, l_height;
+        int r_height, l_height;
 
-	if (!tree)
-		return (0);
-	r_height = _binary_tree_height(tree->right);
-	l_height = _binary_tree_height(tree->left);
-	return (1 + ((r_height >= l_height) ? r_height : l_height));
+        if (!tree)
+                return (0);
+        r_height = _binary_tree_height(tree->right);
+        l_height = _binary_tree_height(tree->left);
+        return (1 + ((r_height >= l_height) ? r_height: l_height));
 }
 
 /**
@@ -23,9 +23,9 @@ size_t _binary_tree_height(const binary_tree_t *tree)
  */
 int _binary_tree_balance(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
-	return (_binary_tree_height(tree->left) - _binary_tree_height(tree->right));
+        if (!tree)
+                return (0);
+        return (_binary_tree_height(tree->left) - _binary_tree_height(tree->right));
 }
 
 /**
@@ -35,15 +35,14 @@ int _binary_tree_balance(const binary_tree_t *tree)
  */
 int sub_tree_perfect(const binary_tree_t *tree)
 {
-	if (tree && !tree->right && !tree->left)
-		return (1);
+        if (tree && !tree->right & !tree->left)
+                return (1);
 
-	if (tree && tree->right && tree->left)
-		return (1 && sub_tree_perfect(tree->left)
-				&& sub_tree_perfect(tree->right));
-	return (0);
+        if (tree && tree->right && tree->left)
+                return (1 && sub_tree_perfect(tree->left)
+                && sub_tree_perfect(tree->right));
+        return (0);
 }
-
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: pointer to the root node of the tree to check
@@ -51,11 +50,11 @@ int sub_tree_perfect(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
+        if (!tree)
+                return (0);
 
-	if (_binary_tree_balance(tree) != 0)
-		return (0);
+        if (_binary_tree_balance(tree) != 0)
+                return (0);
 
-	return (sub_tree_perfect(tree->left) && sub_tree_perfect(tree->right));
+        return (sub_tree_perfect(tree->left) && sub_tree_perfect(tree->right));
 }
